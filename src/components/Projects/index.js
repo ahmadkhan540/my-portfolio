6112c4d2a3acfdadf070e0ab40583a9a9fa115b6
ahmadkhan540 <akler5291@gmail.com>
2024-05-31 18:47:26 +0500

    first commit

---
+++ src/components/Projects/index.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import { useState } from 'react'
+import { Container, Wrapper, Title, Desc, CardContainer, ToggleButtonGroup, ToggleButton, Divider } from './ProjectsStyle'
+import ProjectCard from '../Cards/ProjectCards'
+import { projects } from '../../data/constants'
+
+
+const Projects = ({openModal,setOpenModal}) => {
+  const [toggle, setToggle] = useState('all');
+  return (
+    <Container id="projects">
+      <Wrapper>
+        <Title>Projects</Title>
+        <Desc>
+          I have worked on a wide range of projects. Here are some of my semester and final year projects.
+        </Desc>
+        <ToggleButtonGroup >
+          {toggle === 'all' ?
+            <ToggleButton active value="all" onClick={() => setToggle('all')}>All</ToggleButton>
+            :
+            <ToggleButton value="all" onClick={() => setToggle('all')}>All</ToggleButton>
+          }
+          <Divider />
+          {toggle === 'semester project' ?
+            <ToggleButton active value="semester project" onClick={() => setToggle('semester project')}>SEMESTER PROJECTS'S</ToggleButton>
+            :
+            <ToggleButton value="semester project" onClick={() => setToggle('semester project')}>SEMESTER PROJECTS'S</ToggleButton>
+          }
+          <Divider />
+          {toggle === 'fyp' ?
+            <ToggleButton active value="fyp" onClick={() => setToggle('fyp')}>FYP'S</ToggleButton>
+            :
+            <ToggleButton value="fyp" onClick={() => setToggle('fyp')}>FYP'S</ToggleButton>
+          }
+        </ToggleButtonGroup>
+        <CardContainer>
+          {toggle === 'all' && projects
+            .map((project) => (
+              <ProjectCard project={project} openModal={openModal} setOpenModal={setOpenModal}/>
+            ))}
+          {projects
+            .filter((item) => item.category == toggle)
+            .map((project) => (
+              <ProjectCard project={project} openModal={openModal} setOpenModal={setOpenModal}/>
+            ))}
+        </CardContainer>
+      </Wrapper>
+    </Container>
+  )
+}
+
+export default Projects
---
+++ src/data/constants.js
@@ -0,0 +1,263 @@
+export const Bio = {
+  name: "Muhammad Ahmad Khan",
+  roles: [
+    "Fresh Graduate",
+    "Data Scientist",
+    "Frontend Developer",
+    "AI/ML Engineer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/ahmadkhan540/ahmadkhan",
+  resume:
+    "https://drive.google.com/file/d/1jEsydFC9KqKceS_YfG7XTG1h4ly83BYE/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/ahmad-khan-17a102174",
+  insta: "https://www.instagram.com/ahmadk0._?igsh=bmcxYm5ieGNmeXY1/",
+  facebook: "https://www.facebook.com/profile.php?id=100010181323001&mibextid=ZbWKwL",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://git-scm.com/images/logos/downloads/Git-Icon-1788C.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg",
+      },
+      {
+        name: "Figma",
+        image:
+          "https://www.vectorlogo.zone/logos/figma/figma-icon.svg",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://kics.edu.pk/web/wp-content/uploads/2018/02/unnamed.png",
+    role: "Deep Learning Intern",
+    company: "KICS UET Lahore.",
+    date: "June 2023 - Sep 2023",
+    desc: "Work on Graph Embeddings, Apply different models i.e. Node2Sim , Node2Vec, Deepwalk etc. Work on Link Predictions on Graphs.",
+    skills: [
+      "Python",
+      "TensorFlow",
+      "Keras",
+      "Deep Learning",
+      "Graph Embeddings",
+    ],
+    doc: "https://drive.google.com/file/d/1O-epI-OKQVd9ulvqEunZdQCQIA6ELcme/view?usp=sharing",
+    docIcon: "https://img.icons8.com/ios-filled/50/000000/document.png"
+  },
+  {
+    id: 0,
+    img: "https://kics.edu.pk/web/wp-content/uploads/2018/02/unnamed.png",
+    role: "Web Development Intern",
+    company: "KICS UET Lahore.",
+    date: "August 2023 - September 2023",
+    desc: "Working on Front-End Learn HTML, CSS, JavaScript and react.",
+    skills: [
+      "ReactJS",
+      "HTML",
+      "CSS",
+      "JavaScript",
+    ],
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://upload.wikimedia.org/wikipedia/en/thumb/b/b0/University_of_Engineering_and_Technology_Lahore_logo.svg/800px-University_of_Engineering_and_Technology_Lahore_logo.svg.png",
+    school: "University of Engineering and Technology, Lahore.",
+    date: "Oct 2020 - May 2024",
+    grade: "3.50 CGPA",
+    desc: "I have completed my Bachelors in Computer Science at UET, Lahore with CGPA of 3.50. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others.",
+    degree: "B.Sc. Computer Sciene",
+  },
+  {
+    id: 1,
+    img: "https://seeklogo.com/images/P/punjab-group-of-colleges-logo-0F6CF800F3-seeklogo.com.png",
+    school: "Punjab Group of Colleges, Lahore.",
+    date: "2018 - 2020",
+    grade: "85.3%",
+    desc: "I completed my Intermediate at PGC, Lahore, where I studied Science with Computer Science.",
+    degree: "ICS(Physics)",
+  },
+  {
+    id: 2,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR32RiUZpLhTB6WZ4oXzXT73JaE33uxrSx_gigUwO9zjg&s",
+    school: "L.D.A. Model High School Allama Iqbal Town, Lahore.",
+    date: "2016 - 2018",
+    grade: "89.27%",
+    desc: "I completed my Matriculation at L.D.A. Model High School, Lahore, where I studied Science with Computer Science.",
+    degree: "Matriculation",
+  },
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "Graph Analysis",
+    date: "Jun 2023 - Jul 2023",
+    description:
+      "It is a semester project in which we analyse the graph dataset and find the different statistics from it.",
+    image:
+      "https://research.aimultiple.com/wp-content/uploads/2020/01/Graph-Analytics-e1628165157191-824x618.png.webp",
+    tags: [
+      "Python",
+      "Jupyter Notebook",
+      "Graph Algorithms",
+    ],
+    category: "semester project",
+  },
+  {
+    id: 0,
+    title: "Automated Orange Counting",
+    date: "Apr 2023 - May 2023",
+    description:
+      "Developed a machine learning model in which by giving a picture it detects and count the oranges from the tree.",
+    image:
+      "http://www.northescambia.com/wp-content/uploads/2012/02/oranges12-540x277.jpg",
+    tags: ["Python", "Jupyter Notebook", "Tensorflow", "Keras"],
+    category: "semester project",
+  },
+  {
+    id: 1,
+    title: "Factory Management System",
+    date: "June 2021 - Sep 2021",
+    description:
+      "Designed a Factory Management system in which I used all DSA concepts and it is CLI interface project.",
+    image:
+      "https://www.pc-doctor.com/webp_images/screenshots/factory_sysinfo_summary_cli.webp",
+    tags: [
+      "C++",
+      "DSA",
+      "Linked List",
+      "Stack",
+      "Queues",
+      "Arrays",
+    ],
+    category: "semester project",
+  },
+  {
+    id: 2,
+    title: "Tic-Tac-Toe",
+    date: "Jan 2022 - Mar 2022",
+    description:
+      "In this project I used diffeent concepts of Operating system to make this game functional for two players easily.",
+    image:
+      "https://d112y698adiu2z.cloudfront.net/photos/production/software_photos/001/458/877/datas/gallery.jpg",
+    tags: ["Operating System", "Process", "Threads", "C++"],
+    category: "semester project",
+  },
+  {
+    id: 3,
+    title: "Bank Management System",
+    date: "Dec 2020 - Feb 2021",
+    description:
+      "This is also CLI based project in which I used C++ language to develop the bank management system.",
+    image:
+      "https://miro.medium.com/v2/resize:fit:600/1*nrSiWB7JmaRM6HDu4oqBkA.png",
+    tags: ["C++", "Arrays", "Loops", "Programming Fundamentals"],
+    category: "semester project",
+  },
+  {
+    id: 10,
+    title: "Vehicle Management System",
+    date: "Jun 2021 - Aug 2021",
+    description:
+      "A vehicle management system that used concepts of OOP to develop the mangement system to hanlde cars sale and purchase.",
+    image:
+      "https://www.jamfal.com/k/download/screen/2015/09/kashipara.com_image001-jpg.jpg",
+    tags: ["C++", "OOP", "Visual Studio"],
+    category: "semester project",
+  },
+  {
+    id: 4,
+    title: "Learning Managment System",
+    date: "Feb 2021 - May 2021",
+    description:
+      "A Learning Management System based on Database that is used to take out the data using query.",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/whatsapp-clone-rishav.appspot.com/o/Screenshot%20(151).png?alt=media&token=48391593-1ef0-4a8c-a92a-eb82bdf38e89",
+    tags: ["MySql", "Database"],
+    category: "semester project",
+  },
+  {
+    id: 5,
+    title: "Gestational Sac Segmentation using CNN for early pregnancy detection and diagnosis",
+    date: "Jun 2023 - April 2024",
+    description:
+      "A mobile application that integrates with Deep Learning model to segment and then classify the ultrasound image to predict whether it leads towards miscarriage or not in near future.",
+    image:
+      "https://camo.githubusercontent.com/a328255ad96f861f57d25096d28018ab2656c689a1456b0d145764009bed2d1a/68747470733a2f2f666972656261736573746f726167652e676f6f676c65617069732e636f6d2f76302f622f746f646f2d6170702d63386331392e61707073706f742e636f6d2f6f2f53637265656e73686f74253230283938292e706e673f616c743d6d6564696126746f6b656e3d33643335646366322d626666322d343730382d393031632d343232383866383332386633",
+    tags: ["Flutter", "Tensorflow", "Keras", "Deep Learning", "Python", "Colab"],
+    category: "fyp",
+  },
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+];
